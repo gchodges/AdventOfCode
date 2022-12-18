15c9e813045e2cb506a1426b491538f8bde23a8b
Garrett <gchodges@crimson.ua.edu>
2022-12-18 11:58:33 -0500

    Complete Day 1 Challenge 2

--- Day01/day1.cpp
+++ Day01/day1.cpp
@@ -2,31 +2,77 @@
 #include <fstream>
 #include <climits>
 #include <string>
+#include <algorithm>
+#include <vector>
 
 
 using namespace std;
 
+// int main(int argc, char* argv[]){
+//     ifstream myInput("input.txt");
+
+//     int max = INT_MIN;
+//     int total = 0;
+//     int value = 0;
+//     string sValue;
+//     while(!myInput.eof()){
+//         getline(myInput,sValue);
+//         if (sValue.empty()){                         Code that would complete Part I
+//             if (total > max){
+//                 max = total;
+//             }
+//             total = 0;
+//             continue;
+//         }
+//         value = stoi(sValue);
+//         total += value;
+//     }
+
+//     cout << max << endl;
+
+//     return 0;
+// }
+
+
 int main(int argc, char* argv[]){
     ifstream myInput("input.txt");
 
-    int max = INT_MIN;
     int total = 0;
     int value = 0;
     string sValue;
+    vector <int> topThree;
+    for (int i = 0;i<3;i++){
+        getline(myInput,sValue);
+        topThree.push_back(stoi(sValue));
+        //cout << topThree[i] << endl;
+    }
+
+    std::sort(topThree.begin(),topThree.end());
     while(!myInput.eof()){
         getline(myInput,sValue);
         if (sValue.empty()){
-            if (total > max){
-                max = total;
+            int lowest = -1;
+            for (int i = 0;i<3;i++){
+                
+                if(total > topThree[i]){
+                    topThree[i] = total;
+                    break;
+                }
             }
             total = 0;
+            sort(topThree.begin(),topThree.end());
             continue;
         }
         value = stoi(sValue);
         total += value;
     }
+    int bigTotal = 0;
+    for (int i = 0;i<3;i++){
+        bigTotal += topThree[i];
+        cout << topThree[i] << endl;
+    }
 
-    cout << max << endl;
 
+    cout << bigTotal << endl;
     return 0;
 }
